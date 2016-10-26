2b025b9291183966306946bbd60ba0f8c8ec0557
jqute <jqute1@gmail.com>
2016-10-26 23:30:55 +0300

    Initial commit.

---
+++ src/main/java/com/goit/hotelonlinebooking/TestClass.java
@@ -0,0 +1,12 @@
+package com.goit.hotelonlinebooking;
+
+/**
+ * @author jqute
+ */
+public class TestClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hi!");
+    }
+
+}
