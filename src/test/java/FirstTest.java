8e32056b4975e40d6e232ec609c1d44cafab4196
jqute <jqute1@gmail.com>
2016-10-27 08:50:42 +0300

    Added FirstTest.

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author jqute
+ */
+public class FirstTest {
+
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+    }
+
+}
