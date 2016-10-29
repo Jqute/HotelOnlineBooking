c42873c2a5b58173df4f6100cdf27953447b6e05
usonictw <usonictw@bigmir.net>
2016-10-30 00:08:55 +0300

    realized methods for User

--- src/main/java/com/goit/hotelonlinebooking/dao/UserImplDAO.java
+++ src/main/java/com/goit/hotelonlinebooking/dao/UserImplDAO.java
@@ -2,45 +2,40 @@ package com.goit.hotelonlinebooking.dao;
 
 import com.goit.hotelonlinebooking.entity.User;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 /**
  * Created by user on 28.10.2016.
  */
-public class UserImplDAO extends AbstractDAO {
+public class UserImplDAO extends AbstractDAO<User> {
 
-    private List<User> userList;
 
-    public UserImplDAO() {
-        userList = new ArrayList<>();
-    }
 
     public List<User> findUserByName(String name) {
 
-        return userList.stream()
+        return getList().stream()
                 .filter(user -> user.getName().equals(name))
                 .collect(Collectors.toList());
     }
 
     public List<User> findUserByLastName(String lastName) {
 
-        return userList.stream()
+        return getList().stream()
                 .filter(user -> user.getLastName().equals(lastName))
                 .collect(Collectors.toList());
     }
 
     public List<User> findUserByPhone(int phoneNumber) {
 
-        return userList.stream()
+        return getList().stream()
                 .filter(user -> user.getUserPhoneNumber() == phoneNumber)
                 .collect(Collectors.toList());
     }
 
     public List<User> findUserByEmail(String email) {
 
-        return userList.stream()
+        return getList().stream()
                 .filter(user -> user.getEmail().equals(email))
                 .collect(Collectors.toList());
     }
