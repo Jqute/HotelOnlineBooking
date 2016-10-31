3426555d76b12c870656770746fb707d2c814944
usonictw <0937354341us>
2016-10-31 11:40:31 +0200

    testing registration method

--- src/main/java/com/goit/hotelonlinebooking/TestClass.java
+++ src/main/java/com/goit/hotelonlinebooking/TestClass.java
@@ -1,12 +1,31 @@
 package com.goit.hotelonlinebooking;
 
+import com.goit.hotelonlinebooking.controller.TestController;
+import com.goit.hotelonlinebooking.dao.UserDAO;
+
+import java.io.IOException;
+
 /**
  * @author jqute
  */
 public class TestClass {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
+
         System.out.println("Hi!");
+        TestController testController = new TestController();
+
+
+        try {
+            testController.registerUser();
+        }
+        catch (IOException io){
+            System.out.println("error");
+        }
+
+        System.out.println(testController.userDAO.getList());
+
+
     }
 
 }
