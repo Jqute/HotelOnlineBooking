b2a222d0ef50f75eed5bbf5182ebb4fd7153f7b8
AFedosov <andrey.a.fedosov@gmail.com>
2016-11-01 12:16:45 +0200

    RoomDao

--- src/main/java/com/goit/hotelonlinebooking/dao/RoomDAO.java
+++ src/main/java/com/goit/hotelonlinebooking/dao/RoomDAO.java
@@ -5,9 +5,6 @@
 
     import com.goit.hotelonlinebooking.entity.Room;
     import com.goit.hotelonlinebooking.entity.User;
-
-    import java.util.ArrayList;
-    import java.util.Iterator;
     import java.util.List;
     import java.util.stream.Collectors;
 
@@ -20,23 +17,28 @@
         }
 
         private void generateRooms(){
-            int cntRooms = (int)Math.random()*100;
-            System.out.println(cntRooms);
-            save(new Room(1,1,1,100,null));
-            save(new Room(2,1,2,300,null));
-            save(new Room(3,1,3,400,null));
-            save(new Room(4,2,1,1500,null));
-            save(new Room(5,2,1,1500,null));
-            save(new Room(6,2,4,2000,null));
-            save(new Room(7,3,3,2500,null));
-            save(new Room(8,3,3,2500,null));
+            int maxPrice =  50;
+            int minPrice = 5;
+            int index = 0;
+            int cntFloor = 3 + (int)(Math.random() * ((8 - 3) + 1));
+            int cntRoom = 1 + (int)(Math.random() * ((4 - 1) + 1));
+
+
+            for (int i = 1; i<= cntFloor; i++) {
+                for (int k = 1; k <= cntRoom; k++) {
+                    int capacity = 1 + (int)(Math.random() * ((4 - 1) + 1));
+                    int price = (minPrice*capacity + (int) (Math.random() * ((maxPrice*capacity - minPrice*capacity) + 1)))*100;
+                    index++;
+                    save(new Room(index, price, i, capacity, null));
+                }
+            }
 
         }
 
         public List<Room> getReservedRooms ()
         {
             List<Room> roomList = getList().stream()
-                                            .filter(r -> !r.getUserReserved().equals(null))
+                                            .filter(r -> r.getUserReserved() != null)
                                             .collect(Collectors.toList());
             return roomList;
         }
@@ -44,39 +46,56 @@
         public List<Room> getFreeRooms ()
         {
             List<Room> roomList = getList().stream()
-                                            .filter(r -> r.getUserReserved().equals(null))
+                                            .filter(r -> r.getUserReserved() == null)
                                             .collect(Collectors.toList());
             return roomList;
 
         }
 
-        public Room getRoomById (long id)
+        public Room getRoomById (long Id)
         {
-            Room room = new Room(-1,0,0,0,null);
+
             try
             {
             List<Room> roomList = getList().stream()
-                                            .filter(r -> r.getId() == id)
+                                            .filter(r -> r.getId() == Id)
                                             .collect(Collectors.toList());
-            room = roomList.get(0);
+            Room room  = roomList.get(0);
             }
             catch (IndexOutOfBoundsException | NullPointerException a)
             {
                 System.out.println("Не найдено ниодной комнаты с таким ID");
 
             }
-            return room;
+            return null;
         }
 
-        public boolean checkReservation (long id) {
+        public List<Room> getRoomByUser (User user)
+        {
+            try
+            {
+                List<Room> roomList = getList().stream()
+                        .filter(r -> r.getUserReserved().equals(user))
+                        .collect(Collectors.toList());
+                return roomList;
+            }
+            catch (IndexOutOfBoundsException | NullPointerException a)
+            {
+                System.out.println("Не найдены комнаты забронированные данным пользователем: " + user);
+
+            }
+            return null;
+        }
+
+        public boolean checkReservation (long Id) {
             boolean checked = false;
 
-            Room room = getRoomById(id);
+            Room room = getRoomById(Id);
 
             try
             {
                 User user = room.getUserReserved();
-                if (!user.equals(null))
+                if (user != null)
                     checked = true;
             }
             catch (NullPointerException e)
@@ -87,13 +106,100 @@
 
         }
 
-        public void bookRoom(long roomId,User user)
+        public boolean checkReservation (long Id, User user) {
+            boolean checked = false;
+
+            Room room = getRoomById(Id);
+
+            try
+            {
+                if (user.equals(room.getUserReserved()))
+                checked = true;
+            }
+            catch (NullPointerException e)
+            {
+                checked = false;
+            }
+            return checked;
+
+        }
+
+        public void bookRoom(long Id,User user)
         {
 
         }
-        public void cancelReservation(long roomId, User user)
+        public void cancelReservation(long Id, User user)
         {
 
         }
+
+        public List<Room> findFreeRoomByPrice(int price)
+        {
+            try
+            {
+                List<Room> roomList = getFreeRooms().stream()
+                        .filter(r -> r.getPrice() == price)
+                        .collect(Collectors.toList());
+                return roomList;
+            }
+            catch (IndexOutOfBoundsException | NullPointerException a)
+            {
+                System.out.println("Не найдены свободные комнаты  по цене " + price);
+
+            }
+            return null;
+        }
+
+        public List<Room> findFreeRoomByCapacity(int capacity)
+        {
+            try
+            {
+                List<Room> roomList = getFreeRooms().stream()
+                        .filter(r -> r.getCapacity() == capacity)
+                        .collect(Collectors.toList());
+                return roomList;
+            }
+            catch (IndexOutOfBoundsException | NullPointerException a)
+            {
+                System.out.println("Не найдены свободные комнаты вместимостью: " + capacity);
+
+            }
+            return null;
+        }
+
+        public List<Room> findFreeRoomByFloor(int floor)
+        {
+            try
+            {
+                List<Room> roomList = getFreeRooms().stream()
+                        .filter(r -> r.getFloor() == floor)
+                        .collect(Collectors.toList());
+                return roomList;
+            }
+            catch (IndexOutOfBoundsException | NullPointerException a)
+            {
+                System.out.println("Не найдены свободные комнаты на " + floor + " этаже");
+
+            }
+            return null;
+        }
+
+        public List<Room> findFreeRoomByParams(int price, int capacity)
+        {
+            try
+            {
+                List<Room> roomList = getFreeRooms().stream()
+                        .filter(r -> (r.getPrice() == price) & (r.getCapacity() == capacity))
+                        .collect(Collectors.toList());
+                return roomList;
+            }
+            catch (IndexOutOfBoundsException | NullPointerException a)
+            {
+                System.out.println("Не найдены свободные комнаты по цене " + price + " вместимостью " + capacity);
+
+            }
+            return null;
+        }
+
 
     }
