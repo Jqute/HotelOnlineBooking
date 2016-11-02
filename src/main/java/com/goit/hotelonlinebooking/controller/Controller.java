c7dee07363583cccf7bba77c587c0a8399af6e56
usonictw <0937354341us>
2016-11-02 17:02:45 +0200

    in progress

--- src/main/java/com/goit/hotelonlinebooking/controller/Controller.java
+++ src/main/java/com/goit/hotelonlinebooking/controller/Controller.java
@@ -7,6 +7,7 @@ import com.goit.hotelonlinebooking.entity.User;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Controller {
@@ -29,7 +30,7 @@ public class Controller {
 
         if (hotelList.size() != 0) return hotelList;
         else {
-            System.out.println("user with that Name is not found");
+            System.out.println("Hotel " + hotelName + " is not found");
             return hotelList;
         }
 
@@ -48,6 +49,81 @@ public class Controller {
         }
     }
 
+    public List<Hotel> findRoom(Map<String, String> params) {
+        List<Hotel> workingDB = new ArrayList<>(new HotelDAO().getList());
+
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            String fieldName = entry.getKey();
+            String fieldValue = entry.getValue();
+
+            switch (fieldName) {
+                case "hotelName":
+                    workingDB = workingDB.stream()
+                            .filter(x -> x.getHotelName().equals(fieldValue))
+                            .collect(Collectors.toList());
+                    break;
+
+                case "cityName":
+                    workingDB = workingDB.stream()
+                            .filter(x -> x.getCityName().equals(fieldValue))
+                            .collect(Collectors.toList());
+                    break;
+
+                case "address":
+                    workingDB = workingDB.stream()
+                            .filter(x -> x.getAddress().equals(fieldValue))
+                            .collect(Collectors.toList());
+                    break;
+
+                case "rating":
+                    workingDB = workingDB.stream()
+                            .filter(x -> x.getRating() == Integer.valueOf(fieldValue))
+                            .collect(Collectors.toList());
+                    break;
+
+                case "price":
+
+                    workingDB = workingDB.stream().filter(hotel -> {
+                        List<Room> list = hotel.getRooms();
+                        list = list.stream()
+                                .filter(x -> x.getPrice() == Integer.valueOf(fieldValue))
+                                .collect(Collectors.toList());
+                        long foundEntries = list.size();
+
+                        if (foundEntries == 0) {
+                            return false;
+                        } else {
+                            hotel.setRooms(list);
+                            return true;
+                        }
+                    }).collect(Collectors.toList());
+                    break;
+
+                case "capacity":
+                    workingDB = workingDB.stream().filter(hotel -> {
+                        List<Room> list = hotel.getRooms();
+                        list = list.stream()
+                                .filter(x -> x.getCapacity() == Integer.valueOf(fieldValue))
+                                .collect(Collectors.toList());
+                        long foundEntries = list.size();
+
+                        if (foundEntries == 0) {
+                            return false;
+                        } else {
+                            hotel.setRooms(list);
+                            return true;
+                        }
+                    }).collect(Collectors.toList());
+                    break;
+
+                default:
+                    System.out.println("Parameter \'" + fieldName + "\' given to method FindRoom() is wrong. Interrupted");
+                    return null;
+            }
+        }
+        return workingDB;
+    }
+
     public static void main(String[] args) {
 
         //HotelDAO hotelDAO = new HotelDAO();
@@ -55,14 +131,18 @@ public class Controller {
 
         Controller controller = new Controller();
 
-        controller.userRegistration(new User("Vladimir", "Mischenko", 37, "usonictw@bigmir.net", "0937354341", "123"));
+        controller.userRegistration(new User("Vladimir", "Mischenko", 35, "usonictw@bigmir.net", "0937354341", "123"));
         controller.userRegistration(new User("Ivan", "Ivanov", 16, "ivanov@bigmir.net", "0957344444", "123"));
 
-       // controller.findHotelByCity("Kiev");
-
-        for (Room r : roomDAO.getList()){
-            System.out.println(r);
+        for (Hotel h : controller.findHotelByCity("Kiev")){
+            System.out.println(h);
         }
+
+
+
+
+
+
 
     }
 
