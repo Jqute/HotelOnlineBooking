1cc45ee7c1e28c8b7c3d1938163fc846009dcd47
AFedosov <andrey.a.fedosov@gmail.com>
2016-11-05 22:16:26 +0200

    RoomDao

--- src/main/java/com/goit/hotelonlinebooking/dao/HotelDAO.java
+++ src/main/java/com/goit/hotelonlinebooking/dao/HotelDAO.java
@@ -54,7 +54,7 @@ public class HotelDAO extends AbstractDAO<Hotel> {
     }
 
     private List<Room> roomFactory(int numberOfRooms) {     //return Rooms to some Hotel
-        List<Room> DBRoom = roomDAO.getList();  ///maybe static???!!!!
+        List<Room> DBRoom = generateRooms(false);  ///maybe static???!!!! false - static, true - automatic
         List<Room> fillingList = new ArrayList<>();
         if (DBRoom != null) {
 
@@ -73,5 +73,42 @@ public class HotelDAO extends AbstractDAO<Hotel> {
         return fillingList;
 
     }
+    private  List<Room> generateRooms(boolean auto){
+
+        List<Room> roomList = new ArrayList<>();
+
+        if (!auto)
+        {
+            Random random = new Random();
+            roomList.add(new Room(Math.abs(random.nextInt()),100,1,1,null));
+            roomList.add(new Room(Math.abs(random.nextInt()),200,1,2,null));
+            roomList.add(new Room(Math.abs(random.nextInt()),300,1,3,null));
+            roomList.add(new Room(Math.abs(random.nextInt()),100,2,1,null));
+            roomList.add(new Room(Math.abs(random.nextInt()),400,2,4,null));
+            roomList.add(new Room(Math.abs(random.nextInt()),200,3,2,null));
+            roomList.add(new Room(Math.abs(random.nextInt()),300,3,3,null));
+            roomList.add(new Room(Math.abs(random.nextInt()),400,4,4,null));
+            roomList.add(new Room(Math.abs(random.nextInt()),400,4,4,null));
+        }
+        else {
+
+            Random random = new Random();
+            int countOfFloor = 4;
+            int maxPrice = 5;
+            int minPrice = 0;
+            int index = Math.abs(random.nextInt());
+
+            for (int i = 1; i <= countOfFloor; i++) {
+                for (int k = 1; k <= 5; k++) {
+                    int capacity = 1 + (Math.abs(random.nextInt()) * ((4 - 1) + 1));
+                    int price = (minPrice * capacity + (Math.abs(random.nextInt()) * ((maxPrice * capacity - minPrice * capacity) + 1))) * 100;
+                    index++;
+                    roomList.add(new Room(index, price, i, capacity, null));
+                }
+            }
+        }
+        return roomList;
+    }
+
 
 }
--- src/main/java/com/goit/hotelonlinebooking/dao/RoomDAO.java
+++ src/main/java/com/goit/hotelonlinebooking/dao/RoomDAO.java
@@ -13,50 +13,6 @@
 
     public class RoomDAO  extends AbstractDAO<Room>{
 
-        public RoomDAO() {
-            generateRooms(false);
-        }
-
-        private  List<Room> generateRooms(boolean auto){
-
-            List<Room> roomList = new ArrayList<>();
-
-            if (!auto)
-            {
-                Random random = new Random();
-                roomList.add(new Room(Math.abs(random.nextInt()),100,1,1,null));
-                roomList.add(new Room(Math.abs(random.nextInt()),200,1,2,null));
-                roomList.add(new Room(Math.abs(random.nextInt()),300,1,3,null));
-                roomList.add(new Room(Math.abs(random.nextInt()),100,2,1,null));
-                roomList.add(new Room(Math.abs(random.nextInt()),400,2,4,null));
-                roomList.add(new Room(Math.abs(random.nextInt()),200,3,2,null));
-                roomList.add(new Room(Math.abs(random.nextInt()),300,3,3,null));
-                roomList.add(new Room(Math.abs(random.nextInt()),400,4,4,null));
-                roomList.add(new Room(Math.abs(random.nextInt()),400,4,4,null));
-            }
-            else {
-
-                Random random = new Random();
-                System.out.println();
-
-                int countOfFloor = 4;
-                int maxPrice = 5;
-                int minPrice = 0;
-                int index = Math.abs(random.nextInt());
-
-
-                for (int i = 1; i <= countOfFloor; i++) {
-                    for (int k = 1; k <= 5; k++) {
-                        int capacity = 1 + (Math.abs(random.nextInt()) * ((4 - 1) + 1));
-                        int price = (minPrice * capacity + (Math.abs(random.nextInt()) * ((maxPrice * capacity - minPrice * capacity) + 1))) * 100;
-                        index++;
-                        roomList.add(new Room(index, price, i, capacity, null));
-                    }
-                }
-            }
-            return roomList;
-        }
-
 
 
     }
